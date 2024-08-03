85a2e0b7c1013cc8ffa94d1cc8568b003970d278
Joao0607B <joaoufmg06@gmail.com>
2024-08-02 22:33:45 -0300

    Sala.cpp - atualização1

--- Src/Classes/Sala.cpp
+++ Src/Classes/Sala.cpp
@@ -1,6 +1,30 @@
+#include "Sala.h"
 
-#include <iostream>
-#include <string>
-#include "/home/joao-pedro/Área de trabalho/Trabalho_PDS2/Include/Sala.h" // CAMINHO DA MINHA MAQUINA !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+Sala::Sala() : Porta(false), Janela(false), Lampada(false), TV(false), temperatura(20) {
+    // Construtor inicializa os membros com valores padrão.
+}
 
+void Sala::CH_Porta(bool _x) {
+    Porta = _x;
+    std::cout << "Porta " << (Porta ? "aberta" : "fechada") << std::endl;
+}
 
+void Sala::CH_Janela(bool _x) {
+    Janela = _x;
+    std::cout << "Janela " << (Janela ? "aberta" : "fechada") << std::endl;
+}
+
+void Sala::CH_Lampada(bool _x) {
+    Lampada = _x;
+    std::cout << "Lampada " << (Lampada ? "ligada" : "desligada") << std::endl;
+}
+
+void Sala::CH_TV(bool _x) {
+    TV = _x;
+    std::cout << "TV " << (TV ? "ligada" : "desligada") << std::endl;
+}
+
+void Sala::CH_Temperatura(int _x) {
+    temperatura = _x;
+    std::cout << "Temperatura definida para " << temperatura << " graus" << std::endl;
+}

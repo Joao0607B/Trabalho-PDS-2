de566076a85cc9f8d9ab9f09add0190da2f25030
octoposs <otazioh@gmail.com>
2024-08-02 21:18:05 -0300

    Update sensor.cpp

--- Src/Classes/sensor.cpp
+++ Src/Classes/sensor.cpp
@@ -1,6 +1,6 @@
 #include "sensor.h"
 
-bool sensor::sensor_valido(const std::string& nome_sensor)
+bool sensor::sensor_valido(const std::string& nome_sensor) const
 {
     return nome_sensor[0] == 'T' || nome_sensor[0] == 'U' || nome_sensor[0] == 'L' || (nome_sensor[0] == 'E' 
     && nome_sensor[1] == 'B') || nome_sensor[0] == 'G' || nome_sensor[0] == 'C';
@@ -12,7 +12,8 @@ void sensor::adicionar_sensor(const std::string& nome_sensor)
    {
      if(sensor_map_.find(nome_sensor) == sensor_map_.end())
      {
-      sensor_map_[nome_sensor] = 0;
+       sensor_map_[nome_sensor] = 0;
+       sensor_nome_map_["Não atribuído"] = nome_sensor;
      }
     else
     {
@@ -21,28 +22,45 @@ void sensor::adicionar_sensor(const std::string& nome_sensor)
   }
   else
   {
-    std::cout << "Sensor inválido" << std::endl;
+    std::cout << "Nome do sensor inválido" << std::endl;
   }
 }
-  
 
 void sensor::remover_sensor(const std::string& nome_sensor)
 {
   if(sensor_map_.find(nome_sensor) != sensor_map_.end())
   {
     sensor_map_.erase(nome_sensor);
+    for(auto it = sensor_nome_map_.begin(); it != sensor_nome_map_.end(); ++it)
+      {
+        if(it->second == nome_sensor)
+        {
+          sensor_nome_map_.erase(it);
+        }
+      }
+  }
+  else if(sensor_nome_map_.find(nome_sensor) != sensor_nome_map_.end())
+  {
+    sensor_nome_map_.erase(nome_sensor);
+    string aux = sensor_nome_map_.at(nome_sensor);
+    sensor_map_.erase(aux);
   }
   else
   {
     std::cout << "Sensor não encontrado" << std::endl;
-  }   
+  }
 }
 
-int sensor::obter_valor_sensor(const std::string& nome_sensor)
+int sensor::obter_valor_sensor(const std::string& nome_sensor) const
 {
   if(sensor_map_.find(nome_sensor) != sensor_map_.end())
   {
     return sensor_map_[nome_sensor];
+  }
+  else if(sensor_nome_map_.find(nome_sensor) != sensor_nome_map_.end())
+  {
+    string aux = sensor_nome_map_.at(nome_sensor);
+    return sensor_map_[aux];
   }
   else
   {
@@ -57,9 +75,37 @@ void sensor::atualizar_valor_sensor(const std::string& nome_sensor, int valor)
   {
     sensor_map_[nome_sensor] = valor;
   }
+  else if(sensor_nome_map_.find(nome_sensor) != sensor_nome_map_.end())
+  {
+    string aux = sensor_nome_map_.at(nome_sensor);
+    sensor_map_[aux] = valor;
+  }
   else
   {
     std::cout << "Sensor não encontrado" << std::endl;
   }
 }
 
+void sensor::atualizar_nome_sensor(const std::string& nome_antigo_sensor, const std::string& novo_nome)
+{
+  if(sensor_nome_map_.find(nome_antigo_sensor) != sensor_nome_map_.end())
+  {
+    sensor_nome_map_.find(nome_antigo_sensor)->first = novo_nome;
+  }
+  else
+  {
+    std::cout << "Sensor não existente" << std::endl;
+  }
+}
+
+void sensor::atribuir_nome_sensor(const std::string& nome_sensor, const std::string& nome)
+{
+  if(sensor_map_.find(nome_sensor) != sensor_map_.end())
+  {
+    sensor_nome_map_[nome] = nome_sensor;
+  }
+  else
+  {
+    std::cout << "Sensor não existente" << std::endl;
+  }
+}

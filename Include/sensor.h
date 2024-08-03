fcc8b26bd7685fe0930ecad37ed77b536c0427b0
octoposs <otazioh@gmail.com>
2024-08-02 21:17:18 -0300

    Update sensor.h

--- Include/sensor.h
+++ Include/sensor.h
@@ -6,30 +6,37 @@
 #include <string>
 
 // Tipos de sensores:
-// Umidade, Temperatura, Luminosidade, Presença, Gás, Estado Binário, e Costumizado
+// Umidade, Temperatura, Luminosidade, Presença, Gás, Estado Binário
 
 class sensor 
 {
 private:
 
-  std::map<std::string, int> sensor_map_; // Map com o nome dos sensores e seus respectivos valores
+  std::map<std::string, int> sensor_map_; // Map com o nome dos sensores no txt e seus respectivos valores
+  std::map<std::string, std::string> sensor_nome_map_; // Map com o nome dos sensores e seus respectivos nomes no txt (first = nome do sensor second = nome txt)
 
 public:
 
-// Verifica se o nome do sensor recebido está devidamente nomeado para que se saiba o tipo de sensor e retorna true caso esteja:
+// Verifica se o nome do sensor recebido do txt está devidamente nomeado para que se saiba o tipo de sensor e retorna true caso esteja:
 bool sensor_valido(const std::string& nome_sensor);
 
-// Adiciona um sensor com valor inicial 0:
+// Adiciona um sensor a partir do txt com valor inicial 0 e e nome "Não atribuido";
 void adicionar_sensor(const std::string& nome_sensor);
 
-// Remove um sensor existente no map:
+// Remove um sensor existente nos dois map:
 void remover_sensor(const std::string& nome_sensor);
 
-// Retorna o valor do sensor:
+// Retorna o valor do sensor caso exista ou retorna 0 caso contrário:
 int obter_valor_sensor(const std::string& nome_sensor);
 
 // Atualiza o valor do sensor:
 void atualizar_valor_sensor(const std::string& nome_sensor, int valor);
+
+// Atualiza o nome do sensor:
+void atualizar_nome_sensor(const std::string& nome_antigo_sensor, const std::string& novo_nome);
+
+// Associa um nome dado ao sensor pelo usuário a um sensor vindo do txt
+void atribuir_nome_sensor(const std::string& nome_sensor, const std::string& nome);
 
 };
 

23fa19ac3c20b802ccf9d2b242678ffc39a524e3
octoposs <otazioh@gmail.com>
2024-08-03 16:31:18 -0300

    Add files via upload

---
+++ Src/Classes/quarto.cpp
@@ -0,0 +1,100 @@
+#include "quarto.h"
+
+int quarto::quantidade(std::string tipo)
+{
+  int count = 0;
+   for(int i = 0; i < elementos_.size(); i++)
+   {
+    if(elementos_[i].tipo == tipo)
+    {
+      count++; 
+    }
+   }
+  return count;
+}
+
+void quarto::listar(std::string tipo)
+{
+  for(int i = 0; i < elementos_.size(); i++)
+  {
+    if(elementos_[i].tipo == tipo)
+    {
+      std::cout << elementos_[i].nome << std::endl;
+    }
+  }
+}
+
+
+quarto::quarto(std::string nome, int qtd_portas, int qtd_janelas, int qtd_lampadas, int qtd_tvs, int qtd_ar_condicionado, int qtd_umidificador)
+{
+  nome_ = nome;
+  Nomear_Elemento(qtd_portas, "porta");
+  Nomear_Elemento(qtd_janelas, "janela");
+  Nomear_Elemento(qtd_lampadas, "lampada");
+  Nomear_Elemento(qtd_tvs, "tv");
+  Nomear_Elemento(qtd_ar_condicionado, "ar-condicionado");
+  Nomear_Elemento(qtd_umidificador, "umidificador");
+
+  std::cout << "Quarto criado com sucesso!" << std::endl;
+}
+
+void quarto::Nomear_Elemento(int qtd, std::string tipo)
+{
+  for(int i = 0; i < qtd; i++)
+    {
+      std::cout << "Digite o nome da " << tipo << ':' << std::endl;
+      std::cin >> elementos_[i].nome;
+      elementos_[i].tipo = tipo;
+    }
+}
+
+void quarto::atribuir_sensor(const std::string& nome_sensor, const std::string& nome))
+{
+  for(int i = 0; i < elementos_.size(); i++)
+  {
+    if(elementos_[i].nome == nome)
+    {
+      elementos_[i].sensor_associado = nome_sensor;
+    }
+  }
+}
+
+void quarto::remover_sensor(const std::string& nome_sensor)
+{
+  for(int i = 0; i < elementos_.size(); i++)
+  {
+    if(elementos_[i].sensor_associado == nome_sensor)
+    {
+      elementos_[i].sensor_associado = "Não atribuido";
+    }
+    else
+    {
+      std::cout << "Não existe nenhum sensor com esse nome" << std::endl;
+    }
+  }
+}
+
+void quarto::infos_quarto()
+{
+
+  std::cout << "Nome do quarto: " << nome_ << std::endl;
+  std::cout << "Quantidade de portas: " << quantidade("porta") << std::endl;
+  std::cout << "Quantidade de janelas: " << quantidade("janela") << std::endl;
+  std::cout << "Quantidade de lampadas: " << quantidade("lampada") << std::endl;
+  std::cout << "Quantidade de tvs: " << quantidade("tv") << std::endl;
+  std::cout << "Quantidade de ar-condicionados: " << quantidade("ar-condicionado") << std::endl;
+  std::cout << "Quantidade de umidificadores: " << quantidade("umidificador") << std::endl;
+  std::cout << "Portas: " << std::endl;
+  listar("porta");
+  std::cout << "Janelas: " << std::endl;
+  listar("janela");
+  std::cout << "Lampadas: " << std::endl;
+  listar("lampada");
+  std::cout << "Tvs: " << std::endl;
+  listar("tv");
+  std::cout << "Ar-condicionados: " << std::endl;
+  listar("ar-condicionado");
+  std::cout << "Umificadores: " << std::endl
+  lisar("umidificador");
+  
+}

70b1c55d067fbe5a347a2c4b8a7ec7100af65d15
Lucas-Latini <lucaslatini05@gmail.com>
2024-08-04 12:43:56 -0300

    Update comodo.h
    Adicionado alguns comentários para a maior clareza do código

--- Include/comodo.h
+++ Include/comodo.h
@@ -26,19 +26,19 @@ void listar(std::string tipo) const;
 
 public:
 
-//Construtor
+//Construtor da classe principal
 comodo(int qtd_portas, int qtd_janelas, int qtd_lampadas);
 
 // Nomeia cada elemento baseado na quantidade de cada um e seu tipo
 void Nomear_Elemento(int qtd, std::string tipo);
 
-// Atribui um sensor a um elemento do quarto
+// Atribui um sensor à um elemento do quarto
 void atribuir_sensor(const std::string& nome_sensor, const std::string& nome);
 
 // Remove um sensor de um elemento do quarto
 void remover_sensor(const std::string& nome_sensor);
 
-// Printa as informações do quarto tais como: suas quantidades de elementos e respectivos nomes
+// Printa as informações do quarto tais como: suas quantidades de elementos presentes e respectivos nomes
 void info(std::vector<comodo::elemento> comodos) const;
 
 // Retorna o nome do sensor associado ao elemento
@@ -50,11 +50,12 @@ std::string get_sensor_associado(const std::string& nome) const;
 class quarto : public comodo
 {
   public:
+    //Construtor da classe derivada da classe base "comodo"
     quarto (int qtd_portas, int qtd_janelas, int qtd_lampadas, int qtd_tvs, int qtd_ar_condicionado, int qtd_umidificador) : comodo(qtd_portas, qtd_janelas, 
              qtd_lampadas)
     {
       Nomear_Elemento(qtd_tvs, "tv");
-      Nomear_Elemento(qtd_ar_condicionado, "ar_condicionado");
+      Nomear_Elemento(qtd_ar_condicionado, "ar_condicionado"); //Possibilita nomear os objetos conforme solicitado
       Nomear_Elemento(qtd_umidificador, "umidificador");
     }
 
@@ -64,11 +65,12 @@ class quarto : public comodo
 class cozinha : public comodo
 {
   public:
+    //Construtor da classe derivada da classe base "comodo"
     cozinha(int qtd_portas, int qtd_janelas, int qtd_lampadas, int qtd_gas, int qtd_ar_condicionado, int qtd_umidificador) : comodo(qtd_portas, qtd_janelas, 
             qtd_lampadas)
     {
-      Nomear_Elemento(qtd_ar_condicionado, "ar_condicionado");
-      Nomear_Elemento(qtd_umidificador, "umidificador");
+      Nomear_Elemento(qtd_ar_condicionado, "ar_condicionado"); //Possibilita nomear os objetos conforme solicitado
+      Nomear_Elemento(qtd_umidificador, "umidificador"); 
       Nomear_Elemento(qtd_gas, "gas");  
     }
 
@@ -78,26 +80,27 @@ class cozinha : public comodo
 class sala : public comodo
 {
   public:
+    //Construtor da classe derivada da classe base "comodo"
      sala(int qtd_portas, int qtd_janelas, int qtd_lampadas, int qtd_tvs, int qtd_ar_condicionado, int qtd_umidificador) : comodo(qtd_portas, qtd_janelas, 
           qtd_lampadas)
      {
        Nomear_Elemento(qtd_tvs, "tv");
-       Nomear_Elemento(qtd_ar_condicionado, "ar_condicionado");
+       Nomear_Elemento(qtd_ar_condicionado, "ar_condicionado"); //Possibilita nomear os objetos conforme solicitado
        Nomear_Elemento(qtd_umidificador, "umidificador");
      }
 
     void info(std::vector<comodo::elemento> comodos) const;
 };
 
-class banheiro : public comodo
-{
+class banheiro : public comodo{
+    //Construtor da classe derivada da classe base "comodo"
   banheiro(int qtd_portas, int qtd_janelas, int qtd_lampadas) : comodo(qtd_portas, qtd_janelas, qtd_lampadas){}
 
   void info(std::vector<comodo::elemento> comodos) const;
 };
 
 class garagem : public comodo
-{
+{    //Construtor da classe derivada da classe base "comodo"
   garagem(int qtd_portas, int qtd_janelas, int qtd_lampadas) : comodo(qtd_portas, qtd_janelas, qtd_lampadas) {}
 
   void info(std::vector<comodo::elemento> comodos) const;
